9c15f4d0bd88abbc53d1b8426f803c871aef0e2d
Vinicius Parente <vd.parente@hotmail.com>
2023-10-14 16:15:14 -0300

    Include ResponseEntity

--- src/main/java/br/com/viniciusparente/todolist/user/UserController.java
+++ src/main/java/br/com/viniciusparente/todolist/user/UserController.java
@@ -1,6 +1,8 @@
 package br.com.viniciusparente.todolist.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +16,13 @@ public class UserController {
     private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
         var user = this.userRepository.findByUsername(userModel.getUsername());
 
         if(user != null){
-            System.out.println("Usuário já Existe!");
-            return null;
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já Existe!");
         }
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
