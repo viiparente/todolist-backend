eaf64fa5eddf7c5c740c9fae2d7032c889080821
Vinicius Parente <vd.parente@hotmail.com>
2023-10-14 15:31:17 -0300

    Include Lombok Dependecy

--- src/main/java/br/com/viniciusparente/todolist/user/UserModel.java
+++ src/main/java/br/com/viniciusparente/todolist/user/UserModel.java
@@ -1,32 +1,12 @@
 package br.com.viniciusparente.todolist.user;
 
+import lombok.Data;
+
+@Data
 public class UserModel {
 
     private String username;
     private String name;
     private String password;
-
-    public void setUsername(String username){
-        this.username = username;
-    }
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getName() {
-        return name;
-    }
-
-   
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 }
